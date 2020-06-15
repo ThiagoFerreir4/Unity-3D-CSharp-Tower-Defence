780ab2200a6ff4c09dd9a1de9c5c570ed8a22852
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-15 19:37:18 +0300

    09. Finding game objects by name

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour {
 
+	[SerializeField] Waypoint startWaypoint, endWaypoint;
+
 	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
 	// Use this for initialization
 	void Start () {
 		LoadBlocks();
+		ColorStartAndEnd();
 	}
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+		endWaypoint.SetTopColor(Color.red);
+    }
 
     private void LoadBlocks()
     {
 		var waypoints = FindObjectsOfType<Waypoint>();
 		foreach (Waypoint waypoint in waypoints)			
         {
-			bool isOverlapping = grid.ContainsKey(waypoint.GetGridPos());
-			if (isOverlapping)
+			var gridPos = waypoint.GetGridPos();
+			if (grid.ContainsKey(gridPos))
             {
 				Debug.LogWarning("Skipping overlapping block " + waypoint);
             }
 			else
             {
-				grid.Add(waypoint.GetGridPos(), waypoint);
+				grid.Add(gridPos, waypoint);
 			}			
         }
 		print("Loaded " + grid.Count + " blocks");
